58d3c1d7dea1d9fa176e407bbab8a86dcdd4646e
Kiku <kikuflare@pm.me>
2025-09-13 21:30:37 +0900

    Revert "Increase quantity of Ginseng Concentrate and Bellflower Concentrate from 1 to 10"
    This reverts commit 4b937deeae6cd835e381c2a467373108252b8c4c.

--- src/main/java/scripting/reactor/ReactorActionManager.java
+++ src/main/java/scripting/reactor/ReactorActionManager.java
@@ -177,12 +177,7 @@ public class ReactorActionManager extends AbstractPlayerInteraction {
                     Item drop;
 
                     if (ItemConstants.getInventoryType(d.itemId) != InventoryType.EQUIP) {
-                        short quantity = 1;
-
-                        if (d.itemId == 2022131 || d.itemId == 2022132) {
-                            quantity = 10;
-                        }
-                        drop = new Item(d.itemId, (short) 0, quantity);
+                        drop = new Item(d.itemId, (short) 0, (short) 1);
                     } else {
                         drop = ii.randomizeStats((Equip) ii.getEquipById(d.itemId));
                     }
