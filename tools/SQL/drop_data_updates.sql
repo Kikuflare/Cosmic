f77b972673ad0c3aed1808bdcbca9382ba1e7f28
Kiku <kikuflare@pm.me>
2025-09-13 18:56:26 +0900

    Move fix for quest item not dropping into its own file

---
+++ tools/SQL/drop_data_updates.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    drop_data (
+        dropperid,
+        itemid,
+        minimum_quantity,
+        maximum_quantity,
+        questid,
+        chance
+    )
+VALUES
+    (9300141, 4031698, 1, 1, 3310, 100000);
