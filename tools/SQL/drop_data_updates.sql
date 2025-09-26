d14798efc0ec585de2802bf2536871e6dc9ce991
Kiku <kikuflare@pm.me>
2025-09-26 19:13:35 +0900

    Make mineral and jewel ore drop quantities range to 100

--- tools/SQL/drop_data_updates.sql
+++ tools/SQL/drop_data_updates.sql
@@ -1,11 +1,30 @@
-INSERT INTO
-    drop_data (
-        dropperid,
-        itemid,
-        minimum_quantity,
-        maximum_quantity,
-        questid,
-        chance
-    )
-VALUES
-    (9300141, 4031698, 1, 1, 3310, 100000);
+-- INSERT INTO
+--     drop_data (
+--         dropperid,
+--         itemid,
+--         minimum_quantity,
+--         maximum_quantity,
+--         questid,
+--         chance
+--     )
+-- VALUES
+--     (9300141, 4031698, 1, 1, 3310, 100000);
+
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010000;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010001;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010002;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010003;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010004;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010005;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010006;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4010007;
+
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020000;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020001;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020002;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020003;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020004;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020005;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020006;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020007;
+UPDATE drop_data SET maximum_quantity = 100 WHERE itemid = 4020008;
